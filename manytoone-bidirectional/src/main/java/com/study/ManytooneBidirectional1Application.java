aae1bf112329209dea7199d02d668472d84db891
shiamkumar <41625556+shiamkumar@users.noreply.github.com>
2023-07-06 09:42:33 +0530

    first-commit
    first-commit

---
+++ manytoone-bidirectional/src/main/java/com/study/ManytooneBidirectional1Application.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManytooneBidirectional1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManytooneBidirectional1Application.class, args);
+	}
+
+}
